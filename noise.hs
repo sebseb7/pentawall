bb809cb3bde43f9601ee34e99664affab46d3e41
Astro <astro@spaceboyz.net>
2011-04-06 01:35:03 +0200

    noise: rewrite

--- noise.hs
+++ noise.hs
@@ -1,15 +1,20 @@
 import System.Random
+import Control.Applicative
+
 import LEDWall
 
-stars :: IO [[Color]]
-stars = mapM (\y ->
-               mapM (\x ->
-                     do a <- randomRIO (0, 255)
-                        return $ RGB a (255 - a) 0
-                    ) [1..16]
-             ) [1..15]
-        
---genStars :: [Position]
---genStars = 
-
-main = runAnimation $ stars
+main = do let i :: Int -> Word
+              i = fromIntegral
+          --xs <- map i <$> randomRs (0, 15) <$> newStdGen
+          --ys <- map i <$> randomRs (0, 15) <$> newStdGen
+          colors <- map (\a -> RGB a a a) <$>
+                    map i <$> randomRs (0, 255) <$> newStdGen
+          putStr $
+                 concatMap instructionToCommand $
+                 map (\(xy@(x, y), color) ->
+                       Pixel xy $
+                       if odd (x + y)
+                       then RGB 0 0 0
+                       else RGB 255 255 255
+                     ) $
+                 zip (cycle $ [(x, y) | y <- reverse [1..16], x <- [1..16]]) colors
