6b0791d07543f08bc8c7be15b4f4c1cb61b737a2
john stone <john@tuxcode.org>
2011-05-07 16:42:24 +0200

    jeah.... stil fightig so build

--- clients/libpentawall/src/pentawall.c
+++ clients/libpentawall/src/pentawall.c
@@ -9,11 +9,41 @@
  *        Created:  07.05.2011 15:07:54
  *       Revision:  none
  *       Compiler:  gcc
+ 
  *
  *         Author:  Johannes Steinmetz (js), john@tuxcode.org
  *        Company:  tuxcode.org
  *
  * ============================================================================
  */
+
+
+#include <stdlib.h>
+
+#include "pentawall.h"
+
+struct pentawall_ctx {
+	int i;
+	int o ;
+};
+
+struct pentawall_ctx * pentaw_init(host ,port)
+{
+	pw_ctx_t wall = NULL ;
+	wall = malloc(sizeof(*wall));
+	return wall ;
+}
+
+void pantaw_setpixel(pw_ctx_t wall, wallpixel *p)
+{
+	return ;
+}
+
+void
+pentaw_deinit(pw_ctx_t *wall){
+	free(*wall);
+	*wall = NULL;
+	return ;
+}
 
 
