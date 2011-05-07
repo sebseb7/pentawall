f79f1a8f56d269084b0bdaaf1af82a0ef93f9b95
john stone <john@tuxcode.org>
2011-05-07 15:27:13 +0200

    startet c lib

---
+++ clients/libpentawall/src/pentawall.c
@@ -0,0 +1,19 @@
+/*
+ * ============================================================================
+ *
+ *       Filename:  pentawall.c
+ *
+ *    Description:  
+ *
+ *        Version:  1.0
+ *        Created:  07.05.2011 15:07:54
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Johannes Steinmetz (js), john@tuxcode.org
+ *        Company:  tuxcode.org
+ *
+ * ============================================================================
+ */
+
+
