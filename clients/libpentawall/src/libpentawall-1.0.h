bfe82ba7d31c5565a8af66430bc42d1a1690dc6f
john stone <john@tuxcode.org>
2011-05-07 16:47:29 +0200

    sanitized stuff

---
+++ clients/libpentawall/src/libpentawall-1.0.h
@@ -0,0 +1,33 @@
+/*
+ * ============================================================================
+ *
+ *       Filename:  pentawall.h
+ *
+ *    Description:  
+ *
+ *        Version:  1.0
+ *        Created:  07.05.2011 15:45:12
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Johannes Steinmetz (js), john@tuxcode.org
+ *        Company:  tuxcode.org
+ *
+ * ============================================================================
+ */
+
+#include <stdint.h>
+
+typedef   struct pentawall_ctx *pw_ctx_t;
+
+
+typedef struct {
+	uint16_t x ;
+	uint16_t y ;
+	uint8_t r;
+	uint8_t g;
+	uint8_t b;
+	uint8_t a;
+} wallpixel;
+
+
